2340db53bd8ac912b8c12c2b323985dbddc98b8d
nitron1108 <mja29122@gmail.com>
2025-05-18 17:57:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,9 @@
-// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1]) continue;
     for (int j = i + 1; j < len; ++j) {
+      if (j > i + 1 && arr[j] == arr[j - 1]) continue;
       if (arr[i] + arr[j] == value) {
         count++;
       }
@@ -26,14 +27,12 @@ int countPairs2(int *arr, int len, int value) {
       }
 
       int left_count = 1;
-      while (left + left_count < right && 
-             arr[left] == arr[left + left_count]) {
+      while (left + left_count < right && arr[left] == arr[left + left_count]) {
         left_count++;
       }
 
       int right_count = 1;
-      while (right - right_count > left && 
-             arr[right] == arr[right - right_count]) {
+      while (right - right_count > left && arr[right] == arr[right - right_count]) {
         right_count++;
       }
 
@@ -52,6 +51,7 @@ int countPairs2(int *arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1]) continue;
     int target = value - arr[i];
     int left = i + 1;
     int right = len - 1;
